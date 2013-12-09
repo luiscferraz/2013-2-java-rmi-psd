08c11e1fe9bbc2cec60dc5b8b3db2e7cf8c52f76
humbertobsjunior@gmail.com <humbertobsjunior@gmail.com@55f5c684-e17d-effe-4d56-f79417cb07bb>
2013-12-09 00:53:46 +0000

    Software concluído com controle de login.

--- ModuloCliente/src/gui/SoftwareCliente.java
+++ ModuloCliente/src/gui/SoftwareCliente.java
@@ -1,5 +1,6 @@
 package src.gui;
 
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
@@ -7,25 +8,41 @@ import java.util.Scanner;
 import src.interfaces.IServidorCliente;
 
 public class SoftwareCliente {
-	static Scanner vScanner = new Scanner(System.in);
-	static IServidorCliente vServidor = null;
-	static Registry vRegistry = null;
+	static Scanner aScanner = new Scanner(System.in);
+	static IServidorCliente aIServidorCliente = null; 
+	static Registry aRegistry = null;
+	static boolean aLogado = false;
 
 	public static void main(String[] args) {
-		System.out.println("*** Software Clente ***");
-		
 		try {
-			vRegistry = LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
+			String objName = "rmi://localhost/clienteServer"; //TODO Confirmar nome do Server
+			aIServidorCliente = (IServidorCliente) Naming.lookup(objName);
+			
+			System.out.println("*** Software Clente ***");
+		
+			aRegistry = LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
 		
 			while (true) {
 				try {
 					int vMenuPrincipal = menuPrincipal();
 					if (vMenuPrincipal == 1) {
-						menuConsultarExtrato();
+						if (aLogado) {
+							menuConsultarExtrato();
+						} else {
+							System.out.println("*Precisa estar logado");
+						}
 					} else if (vMenuPrincipal == 2) {
-						vServidor = (IServidorCliente) vRegistry.lookup("efetuarPagamento");
+						if (aLogado) {
+							menuEfetuarPagamento();
+						} else {
+							System.out.println("*Precisa estar logado");
+						}
 					} else if (vMenuPrincipal == 3) {
-						vServidor = (IServidorCliente) vRegistry.lookup("logar");
+						if (!aLogado) {
+							menuLogar();
+						} else {
+							System.out.println("*Login já foi efetuado");
+						}
 					} else if (vMenuPrincipal == 4) {
 						System.out.println("Bye!");
 						break;
@@ -37,30 +54,11 @@ public class SoftwareCliente {
 				}
 			}
 		} catch (Exception e) {
-			System.out.println("*Ocorreu um erro inesperado: " + e.getMessage());
+			System.out.println(e.getMessage());
 		}
-		vScanner.close();
+		aScanner.close();
 	}
 	
-	private static void menuConsultarExtrato() {
-		System.out.print("\nNumero do cartão: ");
-		String vNumeroCartao = vScanner.next();
-		System.out.print("Senha: ");
-		String vSenha = vScanner.next();
-		try {
-			vServidor = (IServidorCliente) vRegistry.lookup("consultarExtrato");
-			String vConsultarExtrato = vServidor.consultarExtrato(vNumeroCartao, vSenha);
-			
-			if (vConsultarExtrato == null) {
-				System.out.println("*Erro: Número do cartão ou senha inválidos");
-			} else {
-				System.out.println("Extrato: " + vConsultarExtrato);
-			}
-		} catch (Exception e) {
-			throw new RuntimeException("*Erro ao consultar extrato" + e.getMessage());
-		}
-	}
-
 	private static int menuPrincipal() throws NumberFormatException
 	{
 		System.out.println("\nEscolha uma opção:");
@@ -69,7 +67,61 @@ public class SoftwareCliente {
 		System.out.println("3- Logar");
 		System.out.println("4- Finalizar Software");
 		
-		String vTexto = vScanner.next();
+		String vTexto = aScanner.next();
 		return Integer.parseInt(vTexto);
+	}
+	
+	private static void menuConsultarExtrato() {
+		System.out.print("\nNúmero do cartão: ");
+		String vNumeroCartao = aScanner.next();
+		System.out.print("Senha: ");
+		String vSenha = aScanner.next();
+		try {
+			String vConsultarExtrato = aIServidorCliente.consultarExtrato(vNumeroCartao, vSenha);
+			
+			if (vConsultarExtrato == null) {
+				System.out.println("*Número do cartão ou senha inválidos");
+			} else {
+				System.out.println("Extrato: " + vConsultarExtrato);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("*Erro ao consultar extrato: " + e.getMessage());
+		}
+	}
+	
+	private static void menuEfetuarPagamento() {
+		System.out.print("\nNúmero do cartão: ");
+		String vNumeroCartao = aScanner.next();
+
+		try {
+			boolean vPagamentoEfetuado = aIServidorCliente.efetuarPagamento(vNumeroCartao);
+			
+			if (vPagamentoEfetuado) {
+				System.out.println("Pagamento efetuado com sucesso!");
+			} else {
+				System.out.println("*Não foi possível efetuar pagamento");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("*Erro ao efetuar pagamento: " + e.getMessage());
+		}
+	}
+	
+	private static void menuLogar() {
+		System.out.print("\nCPF: ");
+		String vCPF = aScanner.next();
+		System.out.print("Senha: ");
+		String vSenha= aScanner.next();
+		
+		try {
+			aLogado = aIServidorCliente.logar(vCPF, vSenha);
+			
+			if (aLogado) {
+				System.out.println("Login efetuado com sucesso!");
+			} else {
+				System.out.println("*Não foi possível efetuar login");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("*Erro ao logar: " + e.getMessage());
+		}
 	}
 }
