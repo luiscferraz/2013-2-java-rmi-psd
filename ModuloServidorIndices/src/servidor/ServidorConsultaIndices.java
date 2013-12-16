f985cea79c3e7b3d1109139db0ad61051b328c25
Erickhaendel@gmail.com <Erickhaendel@gmail.com@55f5c684-e17d-effe-4d56-f79417cb07bb>
2013-12-16 13:34:09 +0000

    Correção na classe

--- ModuloServidorIndices/src/servidor/ServidorConsultaIndices.java
+++ ModuloServidorIndices/src/servidor/ServidorConsultaIndices.java
@@ -10,16 +10,21 @@ import interfaces.IServidorConsultaIndices;
  *
  */
 public class ServidorConsultaIndices implements IServidorConsultaIndices {
+	
+	float dolar = 2.30889826f;
+	float euro = 3.17681312f;
+	float iof = 0.38f;
+	public static void main(String args[]){
+		System.out.println("oi");
+		
+	}
 	/**
 	 * Converter valores de Dolar para real, tendo como base o valor : 2,30889826;
 	 * @param float Valor a ser convetido
 	 * @return float Valor apos conversão;
 	 */
 	public float converterDolarReal(float valor) throws RemoteException{
-		
-		float valor_dolar = 2.30889826f;
-		float valor_fim  = valor_dolar * valor;
-		
+		float valor_fim  = this.dolar * valor;
 		return valor_fim;
 	};
 	/**
@@ -28,13 +33,11 @@ public class ServidorConsultaIndices implements IServidorConsultaIndices {
 	 * @return float Valor apos conversão;
 	 */
 	public float converterEuroReal(float valor) throws RemoteException{
-		float valor_dolar = 3.17681312f;
-		float valor_fim  = valor_dolar * valor;
 		
+		float valor_fim  = this.euro * valor;
 		return valor_fim;
 	};
 	public float consultarIOF() throws RemoteException{
-		float valor = 0.38f;
-		return valor;
+		return this.iof;
 	};
 }
