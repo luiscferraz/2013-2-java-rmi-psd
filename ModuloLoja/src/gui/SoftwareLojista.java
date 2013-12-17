7220913e6d6f4f77440976b9d490d1c0a8659cd8
marcela.domingues@gmail.com <marcela.domingues@gmail.com@55f5c684-e17d-effe-4d56-f79417cb07bb>
2013-12-17 18:33:29 +0000

    Métodos eDevedor e consultarDevedores no SoftwareLojista.

--- ModuloLoja/src/gui/SoftwareLojista.java
+++ ModuloLoja/src/gui/SoftwareLojista.java
@@ -2,6 +2,7 @@ package gui;
 
 import interfaces.IServidorAdministrador;
 import interfaces.IServidorCliente;
+import interfaces.IServidorConsultaCadastroNegativo;
 import interfaces.IServidorLoja;
 
 import java.lang.reflect.Array;
@@ -14,6 +15,7 @@ import java.util.Scanner;
 
 public class SoftwareLojista {
 
+	private static final String IServidorLoja = null;
 	private static Scanner jScanner = new Scanner(System.in);
 	private static IServidorLoja jIServidorLojista = null; 
 	
@@ -21,14 +23,18 @@ public class SoftwareLojista {
 	
 	private static boolean logado = false;
 	private static String host = "172.16.181.128";
+	private static Registry oRegistry;
+	private static IServidorLoja oIServidorLoja;
+	private static IServidorConsultaCadastroNegativo oIServidorConsultaCadastroNegativo;
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		try{
-			//exatamente igual ao que estava no slide
-			String objName = "rmi://localhost/lojistaServer"; //TODO Confirmar nome do Server
-			jIServidorLojista = (IServidorLoja) Naming.lookup(objName);
+			SoftwareLojista.oRegistry = LocateRegistry.getRegistry(SoftwareLojista.host, Registry.REGISTRY_PORT);
+			SoftwareLojista.oIServidorLoja = (IServidorLoja) SoftwareLojista.oRegistry.lookup("servidor_loja");
+			SoftwareLojista.oIServidorConsultaCadastroNegativo = (IServidorConsultaCadastroNegativo) SoftwareLojista.oRegistry.lookup("servidor_consulta_cadastro_negativo");
+			Integer ver = null;
 			
 			System.out.println("*** Software Lojista ***");		
 			jRegistry = LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
@@ -43,6 +49,7 @@ public class SoftwareLojista {
 		System.out.println("\nEscolha uma opção:");
 		System.out.println("1 - Consulta SPC.");
 		System.out.println("2 - Efetuar Venda.");
+		System.out.println("3 - Consulta devedor.");
 		System.out.println("0 - Sair.");
 		System.out.println("Digite:");
 		
@@ -56,15 +63,41 @@ public class SoftwareLojista {
 		}
 	}
 	
-	private static boolean ehDevedor(){
-		return true;		
-	}
+	private static boolean eDevedor(){
+		
+		System.out.print("\nInforme o numero do CPF: ");
+		String cpf = SoftwareLojista.jScanner.next();
+		
+		try{
+			if (SoftwareLojista.oIServidorConsultaCadastroNegativo.eDevedor(cpf)){
+				System.out.println("O CPF" + cpf + "está na lista de cadastros negativos.");
+				return true;
+			}
+			else{
+				System.out.println("Não é devedor.");
+				return false;
+			}
+			
+		}
+		catch(Exception e){
+			throw new RuntimeException("*Erro." + e.getMessage());
+		}
+		
+	}	
 	
-	private static ArrayList<Cliente> consultarDevedores(){
-	
+	private static String consultarDevedores(){
+		
+		try{
+			String devedores = SoftwareLojista.oIServidorConsultaCadastroNegativo.consultarDevedores();
+			return devedores;
+		}
+		catch(Exception e){
+			throw new RuntimeException("*Erro ao listar devedores." + e.getMessage());
+		}
 	}
 	
 	private static boolean efetuarVenda(){
+
 		System.out.print("\nInforme o numero do item a ser comprado: ");
 		
 		System.out.print("\n1. Revista - R$ 11.90\n2. DVD - R$ 29.90\n3. Porta-Retrato - R$ 45.20\n\n ");
@@ -94,7 +127,7 @@ public class SoftwareLojista {
 			System.out.print("\nDescricao: ");
 			String descricao = SoftwareLojista.jScanner.next();
 			
-			if (SoftwareLojista.jIServidorLojista.realizarCompra(cartao, senha, descricao, valor, moeda)) {
+			if (SoftwareLojista.oIServidorLoja.realizarCompra(cartao, senha, descricao, valor, moeda)) {
 				System.out.println("Compra realizada com sucesso!");
 			} else {
 				System.out.println("*Erro na realização da compra!");
@@ -103,6 +136,48 @@ public class SoftwareLojista {
 		} catch (Exception e) {
 			throw new RuntimeException("*Erro ao tentar realizar a compra: " + e.getMessage());
 		}
+
+		System.out.print("\nInforme o numero do item a ser comprado: ");
+		
+		System.out.print("\n1. Revista - R$ 11.90\n2. DVD - R$ 29.90\n3. Porta-Retrato - R$ 45.20\n\n ");
+		opcao = SoftwareLojista.jScanner.next();
+		
+		try {
+			float valor = 0;
+			String moeda = "Real";
+			
+			if (opcao == "1") {
+				valor = (float) 11.9;
+				moeda = "Real";
+			} else if (opcao == "2") {
+				valor = (float) 29.9;
+				moeda = "Real";
+			} else if (opcao == "3") {
+				valor = (float) 45.2;
+				moeda = "Real";
+			} else {
+				System.out.print("\nOpção incorreta: ");
+			}
+			
+			System.out.print("\nInforme o numero do cartao: ");
+			String cartao = SoftwareLojista.jScanner.next();
+			System.out.print("\nSenha: ");
+			String senha = SoftwareLojista.jScanner.next();
+			System.out.print("\nDescricao: ");
+			String descricao = SoftwareLojista.jScanner.next();
+			
+			if (SoftwareLojista.jIServidorLojista.realizarCompra(cartao, senha, descricao, valor, moeda)) {
+				System.out.println("Compra realizada com sucesso!");
+				return true;
+			} else {
+				System.out.println("*Erro na realização da compra!");
+			}
+			
+		} catch (Exception e) {
+			throw new RuntimeException("*Erro ao tentar realizar a compra: " + e.getMessage());
+		}
+		return logado;
+		
 	}
 	
 }
