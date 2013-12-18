bb1a5c293cac8220ffeeb63d8347b94c3a83a739
rodrigofelixrodrigo@gmail.com <rodrigofelixrodrigo@gmail.com@55f5c684-e17d-effe-4d56-f79417cb07bb>
2013-12-18 13:32:00 +0000

    Tirando as excessões servidor de índice

--- trunk/ModuloServidorIndices/src/principal/ServidorIndices.java
+++ trunk/ModuloServidorIndices/src/principal/ServidorIndices.java
@@ -5,20 +5,15 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 import servidor.ServidorConsultaIndices;
-import servidor.ServidorConsultaIndicesREST;
 
 public class ServidorIndices {
 
 	private ServidorConsultaIndices servidorConsultaIndices;
-	// private ServidorConsultaIndicesREST servidorConsultaIndicesREST;
 	
 	public ServidorIndices() {
-		try {
+		
 			this.servidorConsultaIndices = new ServidorConsultaIndices();
-			// this.servidorConsultaIndicesREST = new ServidorConsultaIndicesREST();
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	public static void main(String[] args) {
@@ -30,11 +25,6 @@ public class ServidorIndices {
 			System.out.println("registrando " + nomeServidorIndices + "...");
 			r.rebind(nomeServidorIndices, servidorIndices.servidorConsultaIndices);
 			System.out.println(nomeServidorIndices + " registrado!");
-			// registro do servidor de indices que usa REST
-			String nomeServidorIndicesREST = "servidor_indices_rest";
-			System.out.println("registrando " + nomeServidorIndicesREST + "...");
-			r.rebind(nomeServidorIndicesREST, servidorIndices.servidorConsultaIndicesREST);
-			System.out.println(nomeServidorIndicesREST + " registrado!");
 		}
 		catch (Exception e) {
 			System.err.println("erro na main()! " + e);
