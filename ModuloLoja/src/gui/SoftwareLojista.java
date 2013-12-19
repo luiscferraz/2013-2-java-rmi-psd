d043f897036325141056803aa31eee31a3a59fc8
marcela.domingues@gmail.com <marcela.domingues@gmail.com@55f5c684-e17d-effe-4d56-f79417cb07bb>
2013-12-19 15:25:11 +0000

    correções

--- ModuloLoja/src/gui/SoftwareLojista.java
+++ ModuloLoja/src/gui/SoftwareLojista.java
@@ -24,10 +24,13 @@ public class SoftwareLojista {
 	private static boolean logado = false;
 	private static String servidorCentral = "172.16.181.128";
 	private static String servidorSPC = "172.16.183.68";
+	private static String servidorAdmin = "172.16.183.68";
 	private static Registry registroCentral;
 	private static Registry registroSPC;
+	private static Registry registroAdmin;
 	private static IServidorLoja oIServidorLoja;
 	private static IServidorConsultaCadastroNegativo oIServidorConsultaCadastroNegativo;
+	private static IServidorAdministrador oIServidorAdministrador;
 
 	
 	public static void main(String[] args) {
@@ -35,15 +38,48 @@ public class SoftwareLojista {
 		try{
 			SoftwareLojista.registroCentral = LocateRegistry.getRegistry(SoftwareLojista.servidorCentral, Registry.REGISTRY_PORT);
 			SoftwareLojista.registroSPC = LocateRegistry.getRegistry(SoftwareLojista.servidorSPC, Registry.REGISTRY_PORT);
+			SoftwareLojista.registroAdmin = LocateRegistry.getRegistry(SoftwareLojista.servidorAdmin, Registry.REGISTRY_PORT);
+			
 			SoftwareLojista.oIServidorLoja = (IServidorLoja) SoftwareLojista.registroCentral.lookup("servidor_loja");
 			SoftwareLojista.oIServidorConsultaCadastroNegativo = (IServidorConsultaCadastroNegativo) SoftwareLojista.registroSPC.lookup("servidor_consulta_cadastro_negativo");
+			SoftwareLojista.oIServidorAdministrador = (IServidorAdministrador) SoftwareLojista.registroAdmin.lookup("servidor_administrador");
 			Integer ver = null;
 			
 			
 			System.out.println("*** Software Lojista ***");		
+
 			jRegistry = LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
 			
+
+			while(true){
+				System.out.println("*** Software Logista ***");
+				while(!SoftwareLojista.logado){
+					SoftwareLojista.logado = menuLogar();
+				}
+				while(SoftwareLojista.logado){
+					switch(ver){
+						case 1:
+							SoftwareLojista.eDevedor();
+							ver = null;
+							break;
+						case 2:
+							SoftwareLojista.efetuarVenda();
+							ver = null;
+							break;
+						case 3:
+							SoftwareLojista.consultarDevedores();
+							ver = null;
+							break;
+						case 0:
+							SoftwareLojista.logado = false;
+							break;
+						default:
+							ver = SoftwareLojista.menuPrincipal();
+					}
+				}
+			}
 			
+
 		} catch (Exception e){
 			System.out.println("Problemas ao executar o lookup: " + e.getMessage());
 		}
@@ -52,9 +88,9 @@ public class SoftwareLojista {
 	
 	private static Integer menuPrincipal(){		
 		System.out.println("\nEscolha uma opção:");
-		System.out.println("1 - Consulta SPC.");
+		System.out.println("1 - Consultar SPC.");
 		System.out.println("2 - Efetuar Venda.");
-		System.out.println("3 - Consulta devedor.");
+		System.out.println("3 - Consultar Devedores.");
 		System.out.println("0 - Sair.");
 		System.out.println("Digite:");
 		
@@ -126,6 +162,29 @@ public class SoftwareLojista {
 			
 		} catch (Exception e) {
 			throw new RuntimeException("*Erro ao tentar realizar a compra: " + e.getMessage());
+		}
+		
+		
+	}
+	
+	private static boolean menuLogar(){
+		System.out.print("\nCPF: ");
+		String verCPF = SoftwareLojista.jScanner.next();
+		System.out.print("Senha: ");
+		String verSenha= SoftwareLojista.jScanner.next();
+		
+		try {
+			
+			if (SoftwareLojista.oIServidorAdministrador.logar(verCPF, verSenha)) {
+				System.out.println("Login efetuado com sucesso!");
+				return true;
+			} else {
+				System.out.println("*Nao foi possível efetuar login!");
+				return false;
+			}
+		} catch (Exception e) {
+			System.out.println("*Erro ao logar: " + e.getMessage());
+			return false;
 		}
 		
 	}
